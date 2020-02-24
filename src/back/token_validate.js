8f341d480428b12c7dd40468990d318550c9989e
niexiaoguang <nxg@lohosoft.com>
2020-02-24 16:54:31 +0800

    nginx reverse proxy done , wechat token auth done

---
+++ src/back/token_validate.js
@@ -0,0 +1,44 @@
+
+
+var http = require("http");
+var url = require("url");
+var crypto = require("crypto");
+
+function sha1(str) {
+    var md5sum = crypto.createHash("sha1");
+    md5sum.update(str);
+    str = md5sum.digest("hex");
+    return str;
+}
+
+function validateToken(req, res) {
+    var query = url.parse(req.url, true).query;
+    //console.log("*** URL:" + req.url);
+    //console.log(query);
+    var signature = query.signature;
+    var echostr = query.echostr;
+    var timestamp = query['timestamp'];
+    var nonce = query.nonce;
+    var oriArray = new Array();
+    oriArray[0] = nonce;
+    oriArray[1] = timestamp;
+    oriArray[2] = "c0cb73f4a56d4cbabbc9401cdf120b09"; //my set token ****
+    oriArray.sort();
+    var original = oriArray.join('');
+    console.log("Original str : " + original);
+    console.log("Signature : " + signature);
+    var scyptoString = sha1(original);
+    if (signature == scyptoString) {
+        res.end(echostr);
+        console.log("Confirm and send echo back");
+    } else {
+        res.end("false");
+        console.log("Failed!");
+    }
+}
+
+
+var webSvr = http.createServer(validateToken);
+webSvr.listen(3000, function() {
+    console.log("Start validate");
+});
--- src/index.js
+++ src/index.js
@@ -1,30 +1,45 @@
 'use strict';
-
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const port = process.env.PORT || 3000;
+const { Wechat } = require('wechat-jssdk');
+
+const wechatConfig = {
+    //set your oauth redirect url, defaults to localhost
+    "wechatRedirectUrl": "http://brender.cn/api/wechat/",
+    "wechatToken": "c0cb73f4a56d4cbabbc9401cdf120b09", //not necessary required
+    "appId": "wx71881af6d1cdaea8",
+    "appSecret": "74b7f36f09f414001fc429c756d62a05",
+    // card: true, //enable cards
+    // payment: true, //enable payment support
+    // merchantId: '', //
+    // paymentSandBox: true, //dev env
+    // paymentKey: '', //API key to gen payment sign
+    // paymentCertificatePfx: fs.readFileSync(path.join(process.cwd(), 'cert/apiclient_cert.p12')),
+    //default payment notify url
+    // paymentNotifyUrl: `http://your.domain.com/api/wechat/payment/`,
+    //mini program config
+    // "miniProgram": {
+    //     "appId": "mp_appid",
+    //     "appSecret": "mp_app_secret",
+    // }
+}
+
+const wx = new Wechat(wechatConfig);
 const app = express();
 
-
-const fs = require('fs');
-// const http = require('http');
-const https = require('https');
-const privateKey = fs.readFileSync('./certs/2_brender.cn.key', 'utf8');
-const certificate = fs.readFileSync('./certs/1_brender.cn_bundle.crt', 'utf8');
-
-const credentials = { key: privateKey, cert: certificate };
+app.get('/api', (req, res) => res.send('Hello World!'));
 
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
-// your express configuration here
-
-// const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+app.get('/api/wechat/get-signature', (req, res) => {
+  wx.jssdk.getSignature(req.query.url).then(signatureData => {
+    res.json(signatureData);
+  });
+});
 
 
 
-httpsServer.listen(port);
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 
-
-// app.listen(port, () => console.log(`App listening on port ${port}!`));

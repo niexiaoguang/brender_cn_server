d4513d8c286e1d18a993dcef50ff2ad173f9d094
niexiaoguang <nxg@lohosoft.com>
2020-02-24 11:52:25 +0800

    https works

--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
+
+const fs = require('fs');
+// const http = require('http');
+const https = require('https');
+const privateKey = fs.readFileSync('./certs/2_brender.cn.key', 'utf8');
+const certificate = fs.readFileSync('./certs/1_brender.cn_bundle.crt', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate };
+
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+// your express configuration here
+
+// const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+
+
+httpsServer.listen(port);
+
+
+
+// app.listen(port, () => console.log(`App listening on port ${port}!`));

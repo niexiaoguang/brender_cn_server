d8af7a1e6e0aad98192babe6b2f757e820de72d4
niexiaoguang <nxg@lohosoft.com>
2020-03-12 15:14:58 +0800

    use amqps://amqp.brender.cn as rabbit url

--- src/test_amqp.js
+++ src/test_amqp.js
@@ -16,14 +16,14 @@ const test = () => {
     
 
 
-    var open = require('amqplib').connect('amqps://182.92.200.86:5671', opts);
+    var open = require('amqplib').connect('amqps://amqp.brender.cn', opts);
     open.then(function(conn) {
 
       console.log('inside open');
       // ... go to town
       conn.createChannel().then(function(ch){
         var q = 'hello';
-        var msg = 'Hello World!';
+        var msg = 'Hello Brender hahaha again!';
     
         var ok = ch.assertQueue(q, {durable: false});
         return ok.then(function(_qok) {

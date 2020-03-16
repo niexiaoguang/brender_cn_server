30f5658bab0d6006e3e48b29385c829df5247aea
niexiaoguang <nxg@lohosoft.com>
2020-03-16 15:30:23 +0800

    move code

--- src/utils/crypt.js
+++ src/utils/crypt.js
@@ -1,3 +1,12 @@
+const md5 = require("blueimp-md5");
+
+const make_uuid_by_openid = (raw) => {
+    return md5(raw).substring(8,24);
+
+
+}
+
+
 const sha1 = (str) => {
     var md5sum = crypto.createHash("sha1");
     md5sum.update(str);
@@ -5,5 +14,5 @@ const sha1 = (str) => {
     return str;
 }
 
-
+exports.make_uuid_by_openid = make_uuid_by_openid;
 exports.sha1 = sha1;

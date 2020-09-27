5a2ef4ef6fd31adbec3adbb19ad28e6df1a9e960
niexiaoguang <nxg@lohosoft.com>
2020-09-27 14:46:20 +0800

    send back username via wechat service

--- src/wechat/handle_req.js
+++ src/wechat/handle_req.js
@@ -22,11 +22,51 @@ function handle_wechat_service_req(req,res) {
 				var result = await xmlTools.toJson(buff);
 
 				console.log(result);
-				
+
+				switch (result.xml.MsgType){
+					case 'event':
+
+						switch (result.xml.EventKey) {
+							case 'V1001_MY_PROJ':
+								resProjLinkByUser(res,result);
+								break;
+
+						}
+
+
+
+
+
+						break;
+				}
+
 			}catch (e) {
 				console.log(e);
 			}
 		})();
 	});
 }
+
+
+function resProjLinkByUser(res,result) {
+	console.log('res proj link by user');
+	var user = result.xml.FromUserName;
+	var resMsg = {
+		xml:{
+			ToUserName:result.xml.FromUserName,
+			FromUserName:result.xml.ToUserName,
+			CreateTime:new Date().getTime(),
+			MsgType:'text',
+			Content:'hello ' + user
+		}
+
+	}
+
+
+	var resMsgXml = xmlTools.toXml(resMsg);
+	console.log(resMsgXml);
+	res.send(resMsgXml);
+}
+
+
 exports.handle_wechat_service_req = handle_wechat_service_req;
